6a99e4bcbab812073a89ce10471e973e2133b015
lorianavuk <lorianavuk@gmail.com>
2025-08-23 11:33:23 +0200

    navbar & footer to app.tsx

--- rentacar/src/App.tsx
+++ rentacar/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Cars from './pages/Cars';
 import CarDetails from './pages/CarDetails';
@@ -9,12 +11,14 @@ function App() {
   return (
     <div className='bg-slate-50'>
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cars' element={<Cars />} />
           <Route path='/cars/:id' element={<CarDetails />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
