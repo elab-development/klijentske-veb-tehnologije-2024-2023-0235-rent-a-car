bc327e5dfa898166d0a200c406428258725780dc
lapaccc <lapac911@gmail.com>
2025-08-25 11:58:51 +0200

    Charts for Popular cars page

--- rentacar/src/App.tsx
+++ rentacar/src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Cars from './pages/Cars';
 import CarDetails from './pages/CarDetails';
 import Contact from './pages/Contact';
+import Popular from './pages/Popular';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/cars' element={<Cars />} />
           <Route path='/cars/:id' element={<CarDetails />} />
           <Route path='/contact' element={<Contact />} />
+          <Route path='/popular' element={<Popular />} />
         </Routes>
         <Footer />
       </Router>
